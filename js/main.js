3e1f73358242b4f325f8cee7a56f2fef659cf3d0
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-20 15:21:24 +0200

    console-comment

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 /*Nome*/
 const nameUtent = prompt('Qual è il tuo nome?');
-console.log('nameUtent');
-console.log(typeof 'nameUtent');
+//console.log('nameUtent');
+//console.log(typeof 'nameUtent');
 
 /*Cognome*/
 const surnameUtent = prompt('Qual è il tuo cognome?');
-console.log('surnameUtent');
-console.log(typeof 'surnameUtent');
+//console.log('surnameUtent');
+//console.log(typeof 'surnameUtent');
 
 /*Colore Preferito*/
 const preferColor = prompt('Qual è il tuo colore preferito?');
-console.log('preferColor');
-console.log(typeof 'preferColor');
+//console.log('preferColor');
+//console.log(typeof 'preferColor');
 
 /*Password Value*/
 const Password = nameUtent + surnameUtent + preferColor;
-console.log('Password');
-console.log(typeof 'Password')
+//console.log('Password');
+//console.log(typeof 'Password')
 
 /*Mostrare risultato schermo*/
 document.getElementById('password') .innerHTML = "La tua password è " + Password
